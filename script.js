2b314638c6037f506d6e2f7dbcb41af4b2dc5436
dimxn <zakermen.d@gmail.com>
2023-05-18 21:48:17 +0300

    fix js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const horoscope = async (number) => {
         return data.horoscopeText;
     } catch (err) {
         console.log(err);
-        return err;
+        return "Спробуйте, ще раз";
     }
 };
 
